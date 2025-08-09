4755a6e25b9a2df8a6451a02818e23389f5b33ca
Harshit6-b <harshitbhalani6@gmail.com>
2025-08-09 22:43:24 +0530

    Update hdmi_loader.v

--- hdmi_loader.v
+++ hdmi_loader.v
@@ -22,47 +22,43 @@ reg [4:0] temp ;
       CD <= 2'b11;
   end 
 // Main logic block to handle pixel counting and synchronization signals
-  always @(posedge pixclk) begin
-	      hcount <= hcount + 1;  // Increment pixel counter on each new clock cycle
+ always @(posedge pixclk) begin
+    // Manage horizontal synchronization signal based on pixel count
+    case (hcount)
+        800: begin
+            hsync <= 1;  
+            VDE   <= 0;
+        end
+        840: begin
+            hsync <= 0;  
+            VDE   <= 0;
+        end
+        968: begin 
+            hsync <= 1;  
+            VDE   <= 0;
+        end
+        1056: begin  
+            VDE    <= 0;
+            hcount <= 0;
+            vcount <= vcount + 1;
 
-	      // Manage horizontal synchronization signal based on pixel count
-	      case (hcount)
-		800: begin
-			hsync <= 1;  // End of active video region, prepare for horizontal blanking	
-			VDE<=0;
-		end
-		840: begin
-			hsync <= 0;  // Horizontal sync pulse start
-			VDE<=0;
-		end
-		968: begin 
-			hsync <= 1;  // Horizontal sync pulse end
-			VDE<=0;
-		end
-		1056: begin  // End of horizontal line, reset counter and update frame count
-		    VDE<=0;
-		    hcount <= 0;  // Reset pixel counter to start a new line
-		    vcount <= vcount + 1;  // Increment line count
+            // Vertical sync
+            case(vcount)
+                601: vsync <= 0;
+                605: vsync <= 1;
+                623: vcount <= 0;
+            endcase
+        end
+        default: VDE <= (hcount < 800) && (vcount < 600);
+    endcase
 
-		    // Manage vertical synchronization signal based on line count
-		    case(vcount)
-		        601: vsync <= 0;  // Vertical sync pulse start
-		        605: vsync <= 1;  // Vertical sync pulse end
-		        623: begin  // End of frame (based on line count)
-		            if(temp == 5'd10) begin  // After 10 frames, switch frame (used for animations)
-		                frame_switch <= ~frame_switch;  // Toggle frame switch signal
-		                temp <= 0;  // Reset temporary counter
-		            end else begin
-		                temp <= temp + 1;  // Increment temporary counter
-		            end
-		            vcount <= 0;  // Reset line count for new frame
-		        end
-		    endcase
-		end
-		default :VDE <= (hcount < 800) && (vcount < 600);
-	      endcase
-	      
-	      CD <= {vsync , hsync};
-	  end
+    CD <= {vsync, hsync};
+
+    // Increment at the end
+    if (hcount != 1056)
+        hcount <= hcount + 1;
+end
+
+	
 endmodule
 
