86e7bea3e3b83ef09f8f3f30b549e539273e1cfc
Harshit6-b <harshitbhalani6@gmail.com>
2025-08-12 19:29:32 +0530

    Update uart_tetris.v

--- uart_tetris.v
+++ uart_tetris.v
@@ -7,49 +7,92 @@ module uart(
 );
 
 initial begin
-    bled = 4'b1111;   
-    rled = 4'b0000;
+    bled = 4'b0000;   
+    rled = 4'b0001;
     gled = 4'b0000;
 end
 
 always @(posedge clk) begin
     if (button == 4'b0001) begin
-        bled <= {1'b0, bled[3:1]};    
-         rled <= {1'b0, rled[3:1]};  
-          gled <= {1'b0, gled[3:1]};  
+        if (bled==4'b0000&&gled==4'b0000) begin
+        	if (rled==4'b0001) begin
+        		rled=4'b1000;
+        	end
+        	else begin
+        		rled={1'b0,rled[3:1]};
+        	end
+        end
+        else if (rled==4'b0000&&gled==4'b0000) begin
+        	if (bled==4'b0001) begin
+        		bled=4'b1000;
+        	end
+        	else begin
+        		bled={1'b0,gled[3:1]};
+        	end
+        end
+        else if (bled==4'b0000&&rled==4'b0000) begin
+        	if (gled==4'b0001) begin
+        		gled=4'b1000;
+        	end
+        	else begin
+        		gled={1'b0,gled[3:1]};
+        	end
+        end
     end
     else if (button == 4'b1000) begin
-        bled <= {bled[2:0], 1'b0};   
-        rled <= {rled[2:0], 1'b0};     
-        gled <= {gled[2:0], 1'b0};   
+        if (bled==4'b0000&&gled==4'b0000) begin
+        	if (rled==4'b0001) begin
+        		rled=4'b1000;
+        	end
+        	else begin
+        		rled={rled[2:0],1'b0};
+        	end
+        end
+        else if (rled==4'b0000&&gled==4'b0000) begin
+        	if (bled==4'b0001) begin
+        		bled=4'b1000;
+        	end
+        	else begin
+        		bled={gled[2:0],1'b0};
+        	end
+        end
+        else if (bled==4'b0000&&rled==4'b0000) begin
+        	if (gled==4'b0001) begin
+        		gled=4'b1000;
+        	end
+        	else begin
+        		gled={gled[3:1],1'b0};
+        	end
+        end
+    end
     end
     else if (button == 4'b0100) begin
-        if (bled==4'b1111) begin
-            rled<=4'b1111;
-            bled<=4'b0000;
-        end
-        else if(rled==4'b1111) begin
-            rled<=4'b0000;
-            gled<=4'b1111;
-        end
-        else begin 
-            gled<=4'b0000;
-            bled<=4'b1111;
-        end     
+       if((~(rled==4'b0000))&&(gled==4'b0000)&&(bled==4'b0000)) begin
+       		gled<=rled;
+       		rled<=4'b0000;
+       	end
+       	else if((~(gled==4'b0000))&&(rled==4'b0000)&&(bled==4'b0000)) begin
+       		bled<=gled;
+       		bled<=4'b0000;
+       	end
+       	else if((~(bled==4'b0000))&&(gled==4'b0000)&&(rled==4'b0000)) begin
+       		rled<=bled;
+       		bled<=4'b0000;
+       	end
     end
     else if (button == 4'b0010) begin
-        if (bled==4'b1111) begin
-            gled<=4'b1111;
-            bled<=4'b0000;
-        end
-        else if(rled==4'b1111) begin
-            rled<=4'b0000;
-            bled<=4'b1111;
-        end
-        else begin 
-            gled<=4'b0000;
-            rled<=4'b1111;
-        end              
+        if((~(bled==4'b0000))&&(gled==4'b0000)&&(rled==4'b0000)) begin
+       		gled<=bled;
+       		bled<=4'b0000;
+       	end
+       	else if((~(rled==4'b0000))&&(gled==4'b0000)&&(bled==4'b0000)) begin
+       		bled<=rled;
+       		rled<=4'b0000;
+       	end
+       	else if((~(gled==4'b0000))&&(bled==4'b0000)&&(rled==4'b0000)) begin
+       		rled<=gled;
+       		gled<=4'b0000;
+       	end              
     end
     else begin
         bled <= bled;
