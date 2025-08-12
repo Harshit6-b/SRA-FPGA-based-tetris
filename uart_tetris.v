24d7f8b55b3796972edb24ec6e26f34545f1e45b
Harshit6-b <harshitbhalani6@gmail.com>
2025-08-12 20:22:55 +0530

    Update uart_tetris.v

--- uart_tetris.v
+++ uart_tetris.v
@@ -10,10 +10,10 @@ reg state=12'b000000000001;
 
 always @(posedge clk) begin
    case (button)
-   	4'b0001:state<={1'b0,state[11:1]};
-   	4'b1000:state<={state[10:0],1'b0};
-   	4'b0100:state<={state[7:0],4'b0000};
-   	4'b0010:state<={4'b0000,state[11:4]};
+   	4'b0001:state<=state>>1;
+   	4'b1000:state<=state<<1;
+   	4'b0100:state<=state<<4;
+   	4'b0010:state<=state>>4;
 endcase
  bled = state[11:8];   
     rled = state[7:4];
