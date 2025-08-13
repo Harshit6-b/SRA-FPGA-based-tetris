beb0757d3e1c39a17d870bb6a3e961ebc885a08b
Harshit6-b <harshitbhalani6@gmail.com>
2025-08-13 18:21:55 +0530

    Update uart_tetris.v

--- uart_tetris.v
+++ uart_tetris.v
@@ -41,27 +41,27 @@ always @(posedge clk) begin
     end
     else if (button == 4'b1000) begin
         if (bled==4'b0000&&gled==4'b0000) begin
-        	if (rled==4'b0001) begin
-        		rled=4'b1000;
+        	if (rled==4'b1000) begin
+        		rled=4'b0001;
         	end
         	else begin
         		rled={rled[2:0],1'b0};
         	end
         end
         else if (rled==4'b0000&&gled==4'b0000) begin
-        	if (bled==4'b0001) begin
-        		bled=4'b1000;
+        	if (bled==4'b1000) begin
+        		bled=4'b0001;
         	end
         	else begin
         		bled={gled[2:0],1'b0};
         	end
         end
         else if (bled==4'b0000&&rled==4'b0000) begin
-        	if (gled==4'b0001) begin
-        		gled=4'b1000;
+        	if (gled==4'b1000) begin
+        		gled=4'b0001;
         	end
         	else begin
-        		gled={gled[3:1],1'b0};
+        		gled={gled[2:0],1'b0};
         	end
         end
     end
@@ -72,7 +72,7 @@ always @(posedge clk) begin
        	end
        	else if((~(gled==4'b0000))&&(rled==4'b0000)&&(bled==4'b0000)) begin
        		bled<=gled;
-       		bled<=4'b0000;
+       		gled<=4'b0000;
        	end
        	else if((~(bled==4'b0000))&&(gled==4'b0000)&&(rled==4'b0000)) begin
        		rled<=bled;
